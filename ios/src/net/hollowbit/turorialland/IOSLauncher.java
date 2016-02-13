7b7e0de3a765f84a7992116f52cf27b5d2644572
vedi0boy <vedioboy07@gmail.com>
2016-02-12 22:06:14 -0500

    Added code from tutorial 1.

---
+++ ios/src/net/hollowbit/turorialland/IOSLauncher.java
@@ -0,0 +1,22 @@
+package net.hollowbit.turorialland;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import net.hollowbit.turorialland.Game;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Game(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
