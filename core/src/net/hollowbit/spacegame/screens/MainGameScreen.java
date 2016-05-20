da71f26a50d2cfaba8087a5d8ed3b2683abb505a
vedi0boy <vedioboy07@gmail.com>
2016-05-20 13:40:11 -0400

    Added code from episode 6.

--- core/src/net/hollowbit/spacegame/screens/MainGameScreen.java
+++ core/src/net/hollowbit/spacegame/screens/MainGameScreen.java
@@ -5,38 +5,51 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import net.hollowbit.spacegame.SpaceGame;
 
 public class MainGameScreen implements Screen {
 	
 	public static final float SPEED = 120;
-
-	Texture img;
+	
+	public static final float SHIP_ANIMATION_SPEED = 0.5f;
+	public static final int SHIP_WIDTH_PIXEL = 17;
+	public static final int SHIP_HEIGHT_PIXEL = 32;
+	public static final int SHIP_WIDTH = SHIP_WIDTH_PIXEL * 3;
+	public static final int SHIP_HEIGHT = SHIP_HEIGHT_PIXEL * 3;
+	
+	
+	Animation[] rolls;
+	
 	float x;
 	float y;
+	int roll;
+	float stateTime;
 	
 	SpaceGame game;
 	
 	public MainGameScreen (SpaceGame game) {
 		this.game = game;
+		y = 15;
+		x = SpaceGame.WIDTH / 2 - SHIP_WIDTH / 2;
+		
+		roll = 2;
+		rolls = new Animation[5];
+		
+		TextureRegion[][] rollSpriteSheet = TextureRegion.split(new Texture("ship.png"), SHIP_WIDTH_PIXEL, SHIP_HEIGHT_PIXEL);
+		
+		rolls[roll] = new Animation(SHIP_ANIMATION_SPEED, rollSpriteSheet[0]);
 	}
 	
 	@Override
 	public void show () {
-		img = new Texture("badlogic.jpg");
+		
 	}
 
 	@Override
 	public void render (float delta) {
-		if (Gdx.input.isKeyPressed(Keys.UP)) {
-			y += SPEED * Gdx.graphics.getDeltaTime();
-		}
-		
-		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
-			y -= SPEED * Gdx.graphics.getDeltaTime();
-		}
-		
 		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
 			x -= SPEED * Gdx.graphics.getDeltaTime();
 		}
@@ -44,12 +57,14 @@ public class MainGameScreen implements Screen {
 		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
 			x += SPEED * Gdx.graphics.getDeltaTime();
 		}
+		
+		stateTime += delta;
 
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		game.batch.begin();
 		
-		game.batch.draw(img, x, y);
+		game.batch.draw(rolls[roll].getKeyFrame(stateTime, true), x, y, SHIP_WIDTH, SHIP_HEIGHT);
 		
 		game.batch.end();
 	}
